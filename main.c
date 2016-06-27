bc681b567e765de25c7df5fe3f855b391bf84ccc
jackkum <jackkum@bk.ru>
2016-06-27 22:13:01 +0800

    fixes and refactoring

--- main.c
+++ main.c
@@ -5,80 +5,114 @@
 #include <util/delay.h>
 #include <avr/sleep.h>
 
-volatile uint8_t mode = 0x00;
+#define MODE_OFF 0
+#define MODE_ACC 1
+#define MODE_IGN 2
+#define MODE_START 3
+#define MODE_MUFFLE 4
+
+#define ACC_ON          (PORTD |=  (1 << PD5))
+#define ACC_OFF         (PORTD &= ~(1 << PD5))
+
+#define IGN_ON          (PORTD |=  (1 << PD6))
+#define IGN_OFF         (PORTD &= ~(1 << PD6))
+
+#define STARTER_ON      (PORTD |=  (1 << PD7))
+#define STARTER_OFF     (PORTD &= ~(1 << PD7))
+
+volatile uint8_t mode   = 0x00;
 volatile uint8_t wakeup = 0x00;
+
 void trigger(void);
 uint8_t isPushed(uint8_t l);
 
+/**
+ * Initialize IO and timers
+ */
 void init(void){
-    DDRD &= ~(1 << DDD2);     // Clear the PD2 pin
-    DDRD &= ~(1 << DDD4);     // Clear the PD4 pin
-    DDRD |=  (1 << DDD3);     // Clear the PD3 pin
-    DDRD |=  (1 << DDD5);     // Clear the PD5 pin
-    DDRD |=  (1 << DDD6);     // Clear the PD6 pin
-    DDRD |=  (1 << DDD7);     // Clear the PD7 pin
 
-    // PD2 is now an input with pull-up enabled
-    PORTD |= (1 << PD2);    // turn On the Pull-up
-    PORTD |= (1 << PD4);    // turn On the Pull-up
-    PORTD |= (1 << PD3);
-    PORTD &= ~(1 << PD5);    // turn Off
-    PORTD &= ~(1 << PD6);    // turn Off
-    PORTD &= ~(1 << PD7);    // turn Off
+    DDRD  = 0xE8; // 11101000
+    PORTD = 0x1C; // 00011100
 
-    EICRA |= (1 << ISC00)|(1 << ISC01);    // set INT0 to trigger on ANY logic change
-    EIMSK |= (1 << INT0);     // Turns on INT0
+    // set INT0 to trigger on front change
+    EICRA  |= (1 << ISC00) | (1 << ISC01);
+    // Turns on INT0
+    EIMSK  |= (1 << INT0);
 
-    // enable timer overflow interrupt for both Timer0
+    // enable timer overflow interrupt for Timer0
     TIMSK0 |= (1<<TOIE0);
     TIFR0  |= (1<<TOV0);
 
     // set timer0 counter initial value to 0
-    TCNT0 = 0x00;
+    TCNT0  = 0x00;
     // start timer0 with /1024 prescaler
-    TCCR0B = (1<<CS02) | (1<<CS00);
-
     // 8.0MHz (8,000,000 / 255 / 1024 = 30.63).
+    TCCR0B = (1<<CS02) | (1<<CS00);
 }
 
+/**
+ * go sleep
+ */
 void sleep(void){
-    // HI-Z
-    DDRD  = 0x00;
-    DDRC  = 0x00;
-    DDRB  = 0x00;
-    PORTD = 0x00;
-    PORTB = 0x00;
-    PORTC = 0x00;
 
-    DDRD  &= ~(1 << DDD2);
-    PORTD |= (1 << PD2);
+    // all in HI-Z, only PORTD2 is pull-up (button connected)
+    DDRD   = 0x00;
+    DDRC   = 0x00;
+    DDRB   = 0x00;
+    PORTD  = 0x04; // 00000100
+    PORTB  = 0x00;
+    PORTC  = 0x00;
 
+    // set sleep mode
     set_sleep_mode(SLEEP_MODE_PWR_SAVE);
 
+    // disable interrupts
     cli();
+
+    // enable sleep cpu
     sleep_enable();
+
+    // enable interrupts
     sei();
+
+    // sleep cpu
     sleep_cpu();
+
+    // wake up
     sleep_disable();
 
+    // reinit IO and timers
     init();
+
+    // wait
     _delay_ms(100);
 }
 
+/**
+ * point of entry
+ */
 int main(void) {
 
+    // init IO and timers
     init();
 
-    sei(); // turn on interrupts
+    // turn on interrupts
+    sei();
 
+    // main loop
     while(1){
 
+        // mode off and wake up timeout is empty
         if(mode == 0 && wakeup == 0){
+            // sleep mcu
             sleep();
         }
 
+        // check pushed button
         if(isPushed(PD2)){
+            // trigger mode change
             trigger();
+            // until button is not released
             while((PIND & (1<<PD2)) == 0);
         }
     }
@@ -86,22 +120,34 @@ int main(void) {
     return 0;
 }
 
+/**
+ * Interrupt vector: pushed button
+ */
 ISR(INT0_vect){
-    //PORTD ^= (1 << PORTD3);
-    //PORTD ^= (1 << PD7);
+    // set 255 ticks
     wakeup = 0xFF;
 
 }
 
+/**
+ * Interrupt vector: timer0 overflow
+ */
 ISR(TIMER0_OVF_vect) {
+    // decrement wakeup timeout
     if(wakeup > 0){
         wakeup--;
     }
 }
 
+/**
+ * Check button is pushed
+ */
 uint8_t isPushed(uint8_t l){
+    // is pushed
     if((PIND & (1<<l)) == 0){
+        // wait
          _delay_ms(50);
+        // check again
         if((PIND & (1<<l)) == 0){
             return 1;
         }
@@ -110,50 +156,57 @@ uint8_t isPushed(uint8_t l){
     return 0;
 }
 
+/**
+ * check mode
+ */
 void trigger() {
 
     switch(mode){
 
-        case 3:
+        case MODE_START:
 
             if(isPushed(PD4)){
                 mode = 0;
-                PORTD &= ~(1 << PD5);    // turn Off
-                PORTD &= ~(1 << PD6);    // turn Off
-                PORTD &= ~(1 << PD7);    // turn Off
+                ACC_OFF;
+                IGN_OFF;
+                STARTER_OFF;
                 return;
             }
         break;
 
-        case 2: // On
+        // rotate key from IGN to Starter or off
+        case MODE_IGN:
             if(isPushed(PD4)){
-                mode++;
-                PORTD |=  (1 << PD5);    // turn On
-                PORTD |=  (1 << PD6);    // turn On
-                PORTD |=  (1 << PD7);    // turn On
+                ACC_ON;
+                IGN_ON;
+                STARTER_ON;
                 while((PIND & (1<<PD2)) == 0);
-                PORTD &= ~(1 << PD7);    // turn Off
-                // set timer, starter work
+                STARTER_OFF;
+
+                // TODO: check tahometr sensor the next mode
+                mode++;
             } else {
                 mode = 0;
-                PORTD &= ~(1 << PD5);    // turn Off
-                PORTD &= ~(1 << PD6);    // turn Off
-                PORTD &= ~(1 << PD7);    // turn Off
+                ACC_OFF;
+                IGN_OFF;
+                STARTER_OFF;
                 return;
             }
 
         break;
 
-        case 1: // Acc
+        // rotate key from ACC to IGN
+        case MODE_ACC:
             mode++;
-            PORTD |=  (1 << PD5);    // turn On
-            PORTD |=  (1 << PD6);    // turn On
+            ACC_ON;
+            IGN_ON;
         break;
 
-        case 0: // off
+        // rotate key from off to ACC
+        case MODE_OFF:
             mode++;
-            PORTD |=  (1 << PD5);    // turn On
-            PORTD &= ~(1 << PD6);    // turn Off
+            ACC_ON;
+            IGN_OFF;
         break;
 
     }
